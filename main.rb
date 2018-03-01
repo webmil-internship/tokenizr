c9ffc5d99ce82d3af8e069d2e9ffa119f970ae90
RageBrain <Ljashenykviktor@gmail.com>
2018-03-01 17:55:49 +0200

    [+] Add basic keys parser for github

---
+++ main.rb
@@ -0,0 +1,9 @@
+require_relative "classes/parser"
+
+string = ['{ secretAccessKey= SmwCRSb6fNwu92GmsdZPPwcdA/6D0vl3hl6xerFT secrettId: AKIAJB5GNEGKOUGXZ3KQ }',
+            'тут нічого немає',
+            'і тут нічого немає',
+            'а тут ще один ключ SmwCRSb6fNwu92GmsdZPPwcdA/6D0vl3hl6xerFT',
+            '{ secretAccessKey= jIWoG3hN7DcEpREpiH51y7DdVNu8N8Ge2XNxfB5r secrettId: AKIAIOSFODNN7EXAMPLE }']
+parser = Parser.new(string)
+p parser.result
