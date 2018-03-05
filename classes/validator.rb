24cb8e976cf2ded1b46c00977cfe55662f5d91fb
simplews <skipenko@simple.if.ua>
2018-03-05 17:32:08 +0200

    Done some refactoring

--- classes/validator.rb
+++ classes/validator.rb
@@ -1,40 +1,39 @@
 # Just a top_level_class_documentation comment :)
 class Validator
-  attr_accessor :access_key, :secret_key, :region, :service, :validation_result
+  attr_accessor :access_key, :secret_key, :region, :service, :output
   CONFIG = YAML.load_file('./conf/config.yml') unless defined? CONFIG
-  def initialize
-    @input_data = [
-      { access_key: CONFIG['aws_access_key_id'],
-        secret_key: CONFIG['aws_secret_access_key'] },
-      { access_key: CONFIG['wrong_aws_access_key_id'],
-        secret_key: CONFIG['wrong_aws_secret_access_key'] },
-      { access_key: CONFIG['second_aws_access_key_id'],
-        secret_key: CONFIG['second_aws_secret_access_key'] }
-    ]
+  def initialize(input_data, service)
+    @input_data = input_data
+    @service = service
     @region = CONFIG['aws_region']
-    @service = CONFIG['service']
+    @output = []
   end
 
   def validate
-    array = []
-    @input_data.each do |hash|
-      hash_data = valid_data(hash[:access_key], hash[:secret_key])
-      array << hash_data if hash_data != false
-    end
-    array
+    validate_amazon if service == 'amazon'
+    # TODO: Add methods for other validations
   end
 
-  def valid_data(access_key, secret_key)
+  private
+
+  def validate_amazon
+    @input_data.each do |hash|
+      hash_data = valid_amazon_data(hash[:access_key], hash[:secret_key])
+      output << hash_data if hash_data != false
+    end
+    output
+  end
+
+  def valid_amazon_data(access_key, secret_key)
     ec2 = Aws::EC2::Client.new(
-      region: region,
-      access_key_id: access_key,
-      secret_access_key: secret_key
+      region: region, access_key_id: access_key, secret_access_key: secret_key
     )
     ec2.describe_regions
     { service: service,
       access_key_id: access_key,
       secret_access_key: secret_key }
-    rescue
-    false
+    rescue # TODO: Investigate what type of error returs Amazon
+      false
+    # TODO: Add resque for Network errors
   end
 end
--- test/validator/validator_test.rb
+++ test/validator/validator_test.rb
@@ -6,7 +6,6 @@ require 'aws-sdk-ec2'
 class ValidatorTest < MiniTest::Test
   CONFIG = YAML.load_file('./conf/config.yml') unless defined? CONFIG
   def setup
-    @validator = Validator.new
     @service = CONFIG['service']
     @aws_region = CONFIG['aws_region']
     @input_data = [
@@ -17,6 +16,7 @@ class ValidatorTest < MiniTest::Test
       { access_key: CONFIG['second_aws_access_key_id'],
         secret_key: CONFIG['second_aws_secret_access_key'] }
     ]
+    @validator = Validator.new(@input_data, @service)
   end
 
   def test_it_exists
@@ -29,17 +29,17 @@ class ValidatorTest < MiniTest::Test
     second_access_key = CONFIG['second_aws_access_key_id']
     second_secret_key = CONFIG['second_aws_secret_access_key']
     assert_equal([
-                    { service: @service, access_key_id: access_key,
-                      secret_access_key: secret_key },
-                    { service: @service, access_key_id: second_access_key,
-                   secret_access_key: second_secret_key }
-                  ],
-                 @validator.validate)
+                   { service: @service, access_key_id: access_key,
+                     secret_access_key: secret_key },
+                   { service: @service, access_key_id: second_access_key,
+                     secret_access_key: second_secret_key }
+                 ], @validator.validate)
   end
 
   def test_it_returns_false_with_wrong_keys
+    skip 'Skipped because of using private method'
     access_key = CONFIG['wrong_aws_access_key_id']
     secret_key = CONFIG['wrong_aws_secret_access_key']
-    assert_equal(false, @validator.valid_data(access_key, secret_key))
+    assert_equal(false, @validator.valid_amazon_data(access_key, secret_key))
   end
 end
