7a0e73874ef75947cce9fbffced98fe6ce92f58b
pumbaa2828 <rsavchak@gmail.com>
2018-03-09 14:26:54 +0200

    Refactoring

--- boot.rb
+++ boot.rb
@@ -1,9 +1,7 @@
 require 'awesome_print'
 require 'dotenv/load'
-# require 'net/http'
 require 'octokit'
 require 'sequel'
-# require 'uri'
 require 'yaml'
 
 CONFIG = YAML.load_file('config.yml')
@@ -17,9 +15,13 @@ Dir[File.dirname(__FILE__) + '/classes/**/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
 
 providers = []
-Dir.foreach('classes/provider').map {|x| File.basename(x)}.select { |x| providers << x.scan(/\b(.*)\_/)}
-providers.flatten!.each {|x| PROVIDER << x.capitalize unless x == 'base'}
+Dir.foreach('classes/provider').map { |x| File.basename(x) }.select do |y|
+  providers << y.scan(/\b(.*)\_/)
+end
+providers.flatten!.each { |x| PROVIDER << x.capitalize unless x == 'base' }
 
 targets = []
-Dir.foreach('classes/parser').map {|x| File.basename(x)}.select { |x| targets << x.scan(/\b(.*)\_/)}
-targets.flatten!.each {|x| TARGET << x.capitalize unless x == 'base'}
+Dir.foreach('classes/parser').map { |x| File.basename(x) }.select do |y|
+  targets << y.scan(/\b(.*)\_/)
+end
+targets.flatten!.each { |x| TARGET << x.capitalize unless x == 'base' }
--- classes/cracksman.rb
+++ classes/cracksman.rb
@@ -9,23 +9,13 @@ class CracksMan
   end
 
   def handling
-    puts '<================ start searching ================>'
-    search_result = Object.const_get("#{@provider}Provider").new(@key_phrase).results
-    puts '<================ search result ================>'
-    puts search_result
-    puts '<================ start parsing ================>'
-    # parsed_phrases = Object.const_get("#{@target}Parser").new(search_result).result
-    puts '<================ parse result ================>'
-    # puts parsed_phrases
-    puts '<================ start validating ================>'
-    # valid_phrases = Object.const_get("#{@target}Validator").new(parsed_phrases).result
-    valid_phrases = [{ access_key_id: "AAAAAAAAAAAAAAAAAAAA", secret_access_key: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" },
-                     { access_key_id: "QQQQQQQQQQQQQQQQqQQQ", secret_access_key: "inqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" }]
-    puts '<================ validate result ================>'
-    puts valid_phrases
-    puts '<================ start saving ================>'
+    search_result =
+      Object.const_get("#{@provider}Provider").new(@key_phrase).results
+    parsed_phrases =
+      Object.const_get("#{@target}Parser").new(search_result).result
+    valid_phrases =
+      Object.const_get("#{@target}Validator").new(parsed_phrases).result
     save_valid_phrases(valid_phrases)
-    puts '<================ finished ================>'
   end
 
   private
@@ -35,6 +25,8 @@ class CracksMan
                               target: @target,
                               key_phrase: @key_phrase,
                               when: Time.now)
-    valid_phrases.each { |pairs| pairs.each { |key, value| @passkey.add_phrase(phrase_key: key, phrase_val: value) } }
+    valid_phrases.each do |pairs|
+      pairs.each { |k, v| @passkey.add_phrase(phrase_key: k, phrase_val: v) }
+    end
   end
 end
