ba9f287d38aada42d308778eb23a30bb63e25a2b
supersem <xsupersemx@gmail.com>
2018-03-11 20:42:57 +0200

    Refactoring: inheritance and testing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 gem 'dotenv'
 gem 'octokit'
 gem 'gitlab'
+gem "minitest"
--- classes/provider/base_provider.rb
+++ classes/provider/base_provider.rb
@@ -7,7 +7,15 @@ class BaseProvider
   end
 
   def results
+    raise 'Key_phrase has invalid format.' unless is_key_phrase_valid?(key_phrase)
     search_word
     save_array
   end
+  
+  private
+
+  def is_key_phrase_valid?(key_phrase)
+    key_phrase.is_a?(String)
+  end
 end
+
