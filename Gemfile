e1db12ca63e6f21ffda2e9e4aefdf41c381b2614
Roman Savchak Dell <rsavchak@gmail.com>
2018-03-13 18:52:55 +0200

    beta final

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'awesome_print'
+
 gem 'dotenv'
 gem 'json'
 gem 'octokit', '~> 4.0'
@@ -9,3 +10,7 @@ gem 'sequel'
 gem 'sinatra'
 gem 'sqlite3'
 gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+gem 'aws-sdk-ec2'
+gem 'minitest-reporters'
+gem 'telegram-bot-ruby'
--- classes/cracksman.rb
+++ classes/cracksman.rb
@@ -11,10 +11,17 @@ class CracksMan
   def handling
     search_result =
       Object.const_get("#{@provider}Provider").new(@key_phrase).results
+    puts "==================================== search_result =========================================="
+    puts search_result
     parsed_phrases =
-      Object.const_get("#{@target}Parser").new(search_result).result
+      Object.const_get("#{@target}Parser").new.result(search_result)
+    puts "==================================== parsed_phrases ==========================================="
+    puts parsed_phrases
     valid_phrases =
-      Object.const_get("#{@target}Validator").new(parsed_phrases).result
+      Object.const_get("#{@target}Validator").new(parsed_phrases).validate
+    puts "==================================== valid_phrases ==========================================="
+    puts valid_phrases
+
     save_valid_phrases(valid_phrases)
   end
 
