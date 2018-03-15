65a70cb75906398e74a09ae4f6949e9e1c997d69
Roman Savchak Dell <rsavchak@gmail.com>
2018-03-15 18:57:45 +0200

    change naming

--- boot.rb
+++ boot.rb
@@ -9,19 +9,19 @@ DB = Sequel.connect(CONFIG['db_file'])
 PROVIDER = %w[None]
 TARGET = %w[None]
 
-Dir[File.dirname(__FILE__) + '/classes/**/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
 
 # Create aarays PROVIDER and PARSER from classes in folders
 # classes/provider and classes/parser
 providers = []
-Dir.foreach('classes/provider').map { |x| File.basename(x) }.select do |y|
+Dir.foreach('lib/providers').map { |x| File.basename(x) }.select do |y|
   providers << y.scan(/\b(.*)\_/)
 end
 providers.flatten!.each { |x| PROVIDER << x.capitalize unless x == 'base' }
 
 targets = []
-Dir.foreach('classes/parser').map { |x| File.basename(x) }.select do |y|
+Dir.foreach('lib/parsers').map { |x| File.basename(x) }.select do |y|
   targets << y.scan(/\b(.*)\_/)
 end
 targets.flatten!.each { |x| TARGET << x.capitalize unless x == 'base' }
---
+++ classes/cracks_man.rb
@@ -0,0 +1,38 @@
+# Main class
+class CracksMan
+  attr_accessor :provider, :target, :key_phrase
+
+  def initialize(provider, target, key_phrase)
+    @provider = provider
+    @target = target
+    @key_phrase = key_phrase
+  end
+
+  def handling
+    search_result =
+      Object.const_get("Providers::#{@provider}Provider").new(@key_phrase).results
+    puts "==================================== search_result =========================================="
+    puts search_result
+    parsed_phrases =
+      Object.const_get("Parsers::#{@target}Parser").new.result(search_result)
+    puts "==================================== parsed_phrases ==========================================="
+    puts parsed_phrases
+    valid_phrases =
+      Object.const_get("Validators::#{@target}Validator").new(parsed_phrases).validate
+    puts "==================================== valid_phrases ==========================================="
+    puts valid_phrases
+    save_valid_phrases(valid_phrases)
+  end
+
+  private
+
+  def save_valid_phrases(valid_phrases)
+    @passkey = Passkey.create(provider: @provider,
+                              target: @target,
+                              key_phrase: @key_phrase,
+                              when: Time.now)
+    valid_phrases.each do |pairs|
+      pairs.each { |k, v| @passkey.add_phrase(phrase_key: k, phrase_val: v) }
+    end
+  end
+end
