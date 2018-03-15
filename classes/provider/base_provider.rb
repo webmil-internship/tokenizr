d008e2e06f63024e74d41b16ff52a2960a53df5a
supersem <xsupersemx@gmail.com>
2018-03-15 19:05:49 +0200

    refactoring

--- classes/provider/base_provider.rb
+++ classes/provider/base_provider.rb
@@ -1,21 +1,25 @@
-class BaseProvider
-  attr_accessor :key_phrase
+module Providers
+  class BaseProvider
+    attr_accessor :key_phrase
 
-  def initialize(key_phrase)
-    @array = []
-    @key_phrase = key_phrase
-  end
+    def initialize(key_phrase)
+      @array = []
+      @key_phrase = key_phrase
+    end
 
-  def results
-    raise 'Key_phrase has invalid format.' unless is_key_phrase_valid?(key_phrase)
-    search_word
-    save_array
-  end
-  
-  private
+    def results
+      raise 'Key_phrase has invalid format.' unless key_phrase.is_a?(String)
+      search_word
+      save_array
+    end
 
-  def is_key_phrase_valid?(key_phrase)
-    key_phrase.is_a?(String)
+    def search_word
+
+    end
+
+    def save_array
+
+    end
   end
 end
 
